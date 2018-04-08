0378f16a51504b8d8942c6025cae2079c4c64d46
somnolentus <jhilgar@ucsd.edu>
2018-04-08 02:11:26 -0700

    Testing edge detection
    A function to apply the sobel method to a grayscale image and view the output.

--- testFunc.m
+++ testFunc.m
@@ -1,3 +1,9 @@
-function testFunc(testArg)
-    sprintf('asdf')
+function testFunc(filename)
+
+    image = rgb2gray(imread(filename));
+    imshow(image);
+    
+    imageEdge = edge(image, 'sobel');
+    imshow(imageEdge);
+    
 end
