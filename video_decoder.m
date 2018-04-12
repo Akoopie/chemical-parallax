5960cc45871a2b2acb236d7c546eeb57c5f3f02f
Akoopie <arvinakoopie@gmail.com>
2018-04-12 14:54:55 -0700

    Created frame extractor function
    Updated video_decoder to a function called videoStills. Contains two variables, filename and the frequency of frames extracted.

--- video_decoder.m
+++
@@ -1,7 +0,0 @@
-video = VideoReader('sugar_cube.mp4');
-n = video.NumberOfFrames;
-for i = 1:5:n
-    frames = read(video,i);
-    imwrite(frames,['frames/Image' int2str(i), '.jpg']);
-    im(i) = image(frames);
-end
