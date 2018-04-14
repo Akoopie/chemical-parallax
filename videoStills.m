2af696925ef5c7638f5b794bd2947f14af803415
Akoopie <arvinakoopie@gmail.com>
2018-04-14 13:56:36 -0700

    Corrected indentation
    Corrected indentation for "for" loop and function. Good habits pay off.

--- videoStills.m
+++ videoStills.m
@@ -3,13 +3,13 @@
 
 function videoStills(filename,freq_frames) %Creates the function with 2 parameters: filename (should be in single quotes) and the frequency of frames generated.
 
-video = VideoReader(filename); %Creates variable 'video' to load in file.
-n = video.NumberOfFrames; %This calculates the total number of frames.
+    video = VideoReader(filename); %Creates variable 'video' to load in file.
+    n = video.NumberOfFrames; %This calculates the total number of frames.
 
-for i = 1:freq_frames:n %This loop extracts stills starting from frame 1 up to the final frame, with the frequency of frames written set by the user.
-    frames = read(video,i); %Variable set at current frame in the loop.
-    imwrite(frames,['frames/Image' int2str(i), '.jpg']); %Writes the image as a jpg files. Needs to be a string so it can be saved.
-    im(i) = image(frames); %Displays the frames extracted in Matlab. This is optional.
-end
+    for i = 1:freq_frames:n %This loop extracts stills starting from frame 1 up to the final frame, with the frequency of frames written set by the user.
+        frames = read(video,i); %Variable set at current frame in the loop.
+        imwrite(frames,['frames/Image' int2str(i), '.jpg']); %Writes the image as a jpg files. Needs to be a string so it can be saved.
+        im(i) = image(frames); %Displays the frames extracted in Matlab. This is optional.
+        end
 
 end
