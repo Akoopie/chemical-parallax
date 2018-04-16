1499939adff9077ae2e945186416726dba01e960
somnolentus <jhilgar@ucsd.edu>
2018-04-15 20:59:45 -0700

    Updated .gitignore
    No video uploads

--- testFunc.m
+++ testFunc.m
@@ -1,9 +1,19 @@
-function testFunc(filename)
+function testFunc(filename, filename2)
 
-    image = rgb2gray(imread(filename));
-    imshow(image);
+    image = imrotate(rgb2gray(imread(filename)),90);
+    image2 = imrotate(rgb2gray(imread(filename2)),90);
     
-    imageEdge = edge(image, 'sobel');
-    imshow(imageEdge);
+    points = detectHarrisFeatures(image);
+    points2 = detectHarrisFeatures(image2);
+    
+    [features1,valid_points1] = extractFeatures(image,points);
+    [features2,valid_points2] = extractFeatures(image2,points2);
+    
+    indexPairs = matchFeatures(features1,features2);
+    matchedPoints1 = valid_points1(indexPairs(1:20:end,1),:);
+    matchedPoints2 = valid_points2(indexPairs(1:20:end,2),:);
+    
+    figure; showMatchedFeatures(image,image2,matchedPoints1,matchedPoints2,'montage');
+    %imshow(imageEdge);
     
 end
